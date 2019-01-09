d70b296cb13b44c9e22f5a96fa94fcfdf741fb34
vidhyaveera <46521003+vidhyaveera@users.noreply.github.com>
2019-01-09 16:22:22 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,8 +1,13 @@
-a1=['a','e','i','o','u']
-b=input("")
-for i in range(len(a1)):
-    if(a1[i]==b):
-        print("vowels")
-        break
+a1=['A','E','I','O','U']
+c=input("")
+b=c.upper()
+if ord(b)>=65 and ord(b)<=90:
+    for i in range(len(a1)):
+        if(a1[i]==b):
+            print("vowels")
+            break
+    else:
+        print("consonants")
 else:
-    print("consonants")
+    print("invalid")
+
