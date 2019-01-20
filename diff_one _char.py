793c29084cf195f4cd267d98133a7a19b3874b56
vidhyaveera <46521003+vidhyaveera@users.noreply.github.com>
2019-01-20 12:22:39 +0530

    Update diff_one _char.py

--- diff_one _char.py
+++ diff_one _char.py
@@ -1,8 +1,9 @@
 s,n=map(str,input().split())
-c=0
+c=0 #declare count =0
 for i in range(0,len(s)):
     for j in range(0,len(n)):
         if  i==j:
+            #check the index
             if s[i]!=n[i]:
                 c+=1
 if c==1:
