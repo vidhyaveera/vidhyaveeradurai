5c78f178956104232010ac97e80fe414e757788d
vidhyaveera <46521003+vidhyaveera@users.noreply.github.com>
2019-01-21 15:39:07 +0530

    Create roman_integer.py

---
+++ roman_integer.py
@@ -0,0 +1,4 @@
+roman={"I":1,"II":2,"III":3,"IV":4,"V":5,"VI":6,"VII":7,"VIII":8,"IX":9,"X":10,"XI":11,"XII":12,"XIII":13,"XIV":14,"XV":15,"XVI":16,"XVII":17,"XVIII":18,"IX":19,"XX":20,"XXI":21,"XXII":22,"XXIII":23,"XXIV":24,"XXV":25,"XXVI":26,"XXVII":27,"XXVIII":28,"XXIX":29,"XXX":30}
+n = input("")
+if n in roman:
+    print(roman[n])
