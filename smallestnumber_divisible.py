96c66667f0f00de56a325fe55d746f9777380022
vidhyaveera <46521003+vidhyaveera@users.noreply.github.com>
2019-01-28 19:18:54 +0530

    Update smallestnumber_divisible.py

--- smallestnumber_divisible.py
+++ smallestnumber_divisible.py
@@ -1,13 +1,7 @@
-l,r = map(int,input().split())
-maxer = l*r
-temp = maxer
-a = min(l,r)
-b = max(l,r)
-if(b%a ==0):
-    print(b)
-else:
-    for i in range(temp,0,-1):
-        if(temp %l == 0 and temp %r == 0):
-            maxer = temp
-        temp-=1
-    print(maxer)
+l,r=map(int,input().split())
+a=l*r
+for i in range(1,a+1):
+      if i%l==0 and i%r==0:
+            print(i)
+            break
+            #vidhya
